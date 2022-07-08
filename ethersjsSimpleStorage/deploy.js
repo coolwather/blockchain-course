09eac6453fc8f8c4a05918988a1ccd3844b68d20
Guilherme Soares <guilherme.soares@iovlabs.org>
2022-07-08 17:12:09 -0300

    Added prettier and readme

--- ethersjsSimpleStorage/deploy.js
+++ ethersjsSimpleStorage/deploy.js
@@ -1,17 +1,26 @@
-const ethers = require('ethers');
-const fs = require('fs-extra');
-const dotenv = require('dotenv');
+const ethers = require("ethers");
+const fs = require("fs-extra");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 async function main() {
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
     const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PRIVATE_KEY_PASSWORD);
+    let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+        encryptedJson,
+        process.env.PRIVATE_KEY_PASSWORD
+    );
     wallet = await wallet.connect(provider);
 
-    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
-    const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
+    const abi = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.abi",
+        "utf8"
+    );
+    const binary = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.bin",
+        "utf8"
+    );
 
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
     console.log("Deploying, please wait...");
@@ -45,7 +54,9 @@ async function main() {
     console.log("Current Favorite Number", updatedFavoriteNumber.toString());
 }
 
-main().then(() => process.exit(0)).catch((error) => {
-    console.log(error);
-    process.exit(1);
-});
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    });
