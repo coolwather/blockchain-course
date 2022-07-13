01d0b4a4c300d506778296c856f4896a8f72d9f5
Guilherme Soares <guilherme.soares@iovlabs.org>
2022-07-13 17:42:13 -0300

    Finished

--- ethersjsSimpleStorage/deploy.js
+++ ethersjsSimpleStorage/deploy.js
@@ -6,12 +6,13 @@ dotenv.config();
 
 async function main() {
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-        encryptedJson,
-        process.env.PRIVATE_KEY_PASSWORD
-    );
-    wallet = await wallet.connect(provider);
+    let wallet = new ethers.Wallet(process.env.PRIVATE_KEY_PASSWORD, provider);
+    // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+    // let wallet = new ethers.Wallet(
+    //     encryptedJson,
+    //     process.env.PRIVATE_KEY_PASSWORD
+    // );
+    // wallet = await wallet.connect(provider);
 
     const abi = fs.readFileSync(
         "./SimpleStorage_sol_SimpleStorage.abi",
@@ -26,6 +27,7 @@ async function main() {
     console.log("Deploying, please wait...");
     const contract = await contractFactory.deploy();
     await contract.deployTransaction.wait(1);
+    console.log(`Contract address: ${contract.address}`);
     // console.log("Here is the deployment transaction: ");
     // console.log(contract.deployTransaction);
     // console.log("Here is the transaction receipt: ");
@@ -49,7 +51,7 @@ async function main() {
     const currentFavoriteNumber = await contract.retrieve();
     console.log("Current Favorite Number", currentFavoriteNumber.toString());
     const transactionResponse = await contract.store("7");
-    transactionResponse.wait(1);
+    await transactionResponse.wait(1);
     const updatedFavoriteNumber = await contract.retrieve();
     console.log("Current Favorite Number", updatedFavoriteNumber.toString());
 }
